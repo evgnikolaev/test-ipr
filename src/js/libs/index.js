e1df2d426e4962b8a313d85e2d537208e3c4de07
Evgeny Nikolaev <e.nikolaev@uplab.ru>
2022-11-23 20:58:26 +0300

    АТОМЫ, фактоид, countupjs, saljs

--- src/js/libs/index.js
+++ src/js/libs/index.js
@@ -1,9 +1,13 @@
 import Validation from './validation';
 import Mask from './imask';
+import Counter from './counter';
+import Sal from './sal';
 
 document.addEventListener('DOMContentLoaded', () => {
 	Validation.init();
 	Mask.init();
+	Counter.init();
+	Sal.init();
 });
 
 window.addEventListener('init.validation', () => {
